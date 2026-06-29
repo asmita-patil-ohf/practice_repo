5f78995fcb423814d9578e706f71e7722800a80a
asmitapatil24 <asmita.patil24@vit.edu>
2026-06-29 22:37:13 +0530

    Performed CRUD for FASTAPI

--- app.py
+++ app.py
@@ -2,29 +2,44 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 app=FastAPI()
 
-class Item(BaseModel):
-    text:str 
-    is_done:bool = False
-
-items=[]
-
-@app.get("/")
-def root():
-    return items
-
-@app.post("/items")
-def create_item(item:Item):
-    items.append(item)
-    return item
-
-@app.get("/items/{item_id}", response_model = Item)
-def get_item(item_id:int)->Item:
-    if item_id < len(items):
-        item=items[item_id]
-        return item
-    else:
-        raise HTTPException (status_code=404, detail="Item not found")
+class Student(BaseModel):
+    std_id:int
+    name:str 
+    roll_no:int
+    grade:str
     
-@app.get("/items", response_model=list[Item])
-def list_items(limit:int=10):
-    return items[0:limit]
+students=[]
+
+def search(student_id):
+    for index,student in enumerate(students):
+        if student.std_id == student_id:
+            return index
+    raise HTTPException(status_code=404, detail="Student not found!")
+        
+@app.get("/")
+def all_students():
+    return students
+
+@app.get("/students/{student_id}", response_model=Student)
+def id_student(student_id:int)->Student:
+        get_index=search(student_id)
+        return students[get_index]
+    
+@app.post("/students")
+def create_student(student:Student):
+    students.append(student)
+    return student
+
+@app.put("/students/{student_id}")
+def update_student(student_id:int,student:Student):
+        update_index=search(student_id)
+        students[update_index]=student
+        return students[update_index]
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id:int):
+        del_index=search(student_id)
+        del students[del_index]
+        return students
+    
+    
